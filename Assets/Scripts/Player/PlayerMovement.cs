e3bf22ddf7c2b05fbcf0fdcefe2f8f9eaf1b22d3
Yornie Westink <jinxie88@live.nl>
2015-06-05 10:58:22 +0200

    MovementAnims

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+
+	[SerializeField] private GameObject _player;
+
+	private float _movementspeed = 2;
+
+	void PlayerMoveInput()
+	{
+
+
+
+		if(Input.GetKeyDown(KeyCode.W))
+		{
+
+		}
+
+		if(Input.GetKeyDown(KeyCode.S))
+		{
+
+		}
+
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+
+		}
+
+		if(Input.GetKeyDown(KeyCode.D))
+		{
+		
+		}
+
+	}
+}
