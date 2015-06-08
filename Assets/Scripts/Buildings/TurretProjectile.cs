ef9fabfde543089a5b3924e462f0b847a372b16a
Yornie Westink <jinxie88@live.nl>
2015-06-08 12:24:49 +0200

    Turret

---
+++ Assets/Scripts/Buildings/TurretProjectile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretProjectile : MonoBehaviour {
+
+	private float _lifeTime;
+	private float _speed = 10f;
+	
+	void Update () 
+	{
+		_lifeTime -= 1 * Time.deltaTime;
+		transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+		
+		if (_lifeTime <= 0f) 
+		{
+			Destroy(this.gameObject);
+		}
+	
+	}
+}
---
+++ Assets/Scripts/Buildings/Wall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour, BuildingInterface
+{
+	private int _health = 100;
+	
+	void Start () 
+	{
+	
+	}
+
+
+	void Update () 
+	{
+		TakeDmg ();
+		OnDeath ();
+	
+	}
+
+	public void TakeDmg()
+	{
+
+	}
+
+	public void OnDeath()
+	{
+
+	}
+}
